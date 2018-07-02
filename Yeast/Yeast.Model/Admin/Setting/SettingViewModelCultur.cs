a109b6e2d58155eb3aaedc5d529d076cdc17b598
Seyed Mojtaba Malekabadi <s.mo.malekabadi@gmail.com>
2018-07-02 17:35:21 +0430

    theme

---
+++ Yeast/Yeast.Model/Admin/Setting/SettingViewModelCultur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yeast.Model.Admin.Setting
+{
+    public class SettingViewModelCultur
+    {
+        public string WebSiteName { get; set; }
+        public string MetaTag { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Phone { get; set; }
+        public string Facebooke { get; set; }
+        public string Instagram { get; set; }
+        public string Twitter { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Yeast/Yeast.Model/FrontEnd/FooterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yeast.Model.Admin.Setting;
+
+namespace Yeast.Model.FrontEnd
+{
+    public class FooterModel
+    {
+        public SettingViewModelCultur settingViewModelCultur { get; set; }
+        public AboutUsViewModel aboutUsViewModel { get; set; }
+    }
+}
